359a27b48251961857b7f6398f959bf96f57469a
Elmer Urrea <urrelmer@colpatria.com>
2020-06-11 18:59:58 -0500

    [MODIFY] tests

--- tests/mocks/user/user.model.mock.js
+++ tests/mocks/user/user.model.mock.js
@@ -1,22 +1,28 @@
 module.exports = {
     user: {
-        _id: "67hbg4567uj78udhgTGf4527",
-        name: "Elmer",
-        username: "Elmer123",
-        password: "myPass"
+        _id: "123456789012345678901234",
+        name: "Jaime",
+        username: "Jaime123",
+        password: "myPassjaim"
     },
     users: [
         {
-            _id: "67hbg4567uj78udhgTGf4527",
+            _id: "1768618771268717621876177",
             name: "Elmer",
-            username: "Elmer123",
-            password: "myPass"
+            username: "Elmer876",
+            password: "elmerrr"
         },
         {
-            _id: "67hbg4567uj7fgdhgTGf4518",
+            _id: "12907234639376238239028379",
             name: "Erick",
             username: "Erick_34",
-            password: "myStringPassword"
+            password: "erick--a"
+        },
+        {
+            _id: "2039482397843433432433333",
+            name: "Elkin",
+            username: "elmer94",
+            password: "elkinarray"
         }
     ]
 }
